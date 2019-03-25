cd191ecd99f1ebbff98b0a46aed39763590d0740
Monkata96 <sieroteev@gmail.com>
2019-03-25 15:19:59 +0200

    second commit

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Signup from "./views/Signup.vue";
+import Signin from "./views/Signin.vue";
 
 Vue.use(Router);
 
@@ -9,18 +11,19 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
+      name: 'Home',
+      path: '/',
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      name: 'Signup',
+      path: '/signup',
+      component: Signup
+    },
+    {
+      name: 'Signin',
+      path: '/signin',
+      component: Signin
     }
   ]
 });
---
+++ src/views/Signup.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+       <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Sign up</v-toolbar-title>
+          </v-toolbar>  
+          <v-card-text>
+            <v-form @submit.prevent="submitSignup">
+              <v-text-field v-model="formData.name" prepend-icon="person" name="name" label="Name" type="text"></v-text-field>
+              <v-text-field v-model="formData.email" prepend-icon="email" name="email" label="Email" type="email"></v-text-field>
+              <v-text-field v-model="formData.password" id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn type="submit" color="primary">Sign up</v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitSignup() {
+      this.$store.dispatch('submitSignup', this.formData);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
