cbd0f27b9f6214fbad56d1ca7bc5427a4237d439
Monkata96 <sieroteev@gmail.com>
2019-04-04 21:35:56 +0300

    Added data and created root node into the Firebase.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,53 @@
 <template>
   <div>
-    <h1>Dashboard</h1>
+    <v-layout justify-center>
+      <v-flex>
+        <v-btn color="info" to="/dashboard/add_post" @click="dashboardHideCnt">
+          Add post
+        </v-btn>
+      </v-flex>
+      <v-flex>
+        <v-btn color="error" to="/dashboard/add_post" @click="dashboardHideCnt">
+          Manage post
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout justify-center class="secondFlex">
+      <v-flex>
+        <router-view></router-view>
+      </v-flex>
+    </v-layout>
+
+    <v-layout class="thirdFlex" v-if="dashboardCntFlag">
+      <v-flex>
+        <v-img :src="dashboardImgSrc"/>
+      </v-flex>
+
+      <v-flex>
+        <span>That is your main dashboard!</span>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  data() {
+    return {
+      dashboardImgSrc:"https://cdn.pixabay.com/photo/2019/03/01/17/49/speed-4028234_960_720.jpg",
+      dashboardCntFlag: true
+    }
+  },
+  methods: {
+    dashboardHideCnt() {
+      this.dashboardCntFlag = false;
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  @import "../styles/components/dashboard.scss";
+</style>
+
